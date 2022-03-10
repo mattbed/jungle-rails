7981aa5670e54ef69161bb112685040c221516f8
Matthew <m.bednarski@gmail.com>
2022-03-10 07:38:55 +0000

    Implement basic functionality for dashboard counter

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['Basic_Auth_User'] , password: ENV['Basic_Auth_PW']
   
   def show
+    @products = Product.count
+    @categories = Product.distinct.count(:category_id)
   end
 end
