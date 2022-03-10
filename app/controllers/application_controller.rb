b1ccc2b2e052268541b95b8ef9784f720d3ff35b
Matthew <m.bednarski@gmail.com>
2022-03-10 05:02:15 +0000

    Update order page to show order details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,11 @@ class ApplicationController < ActionController::Base
   end
   helper_method :enhanced_cart
 
+  # def enhanced_order
+  #   @enhanced_order ||= Product.where(id: @order.id).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+  # end
+  # helper_method :enhanced_order
+
   def cart_subtotal_cents
     enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
   end
