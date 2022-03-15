e183962c1898079a4e730272183f38f707b7b7eb
Matthew <m.bednarski@gmail.com>
2022-03-15 07:05:13 +0000

    Refactored

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,47 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    context 'Checks that a product with all fields set' do
-      it 'saves properly' do
+    context 'Checks that an attempt to add product' do
+      it 'saves properly with all required fields' do
         Category.new(name: "test").save
         @category = Category.first.id
         @product = Product.new(name: "test", category_id: @category, quantity: 1, price: 1).save
         expect(@product).to eq(true)
       end
-    end
 
-    context 'Tries to save with a nil-value name' do
-      it 'validates "name"' do
+      it 'validation rejects an empty "name" field' do
         Category.new(name: "test").save
         @category = Category.first.id
         @product = Product.new(name: nil, category_id: @category, quantity: 1, price: 1)
         expect(@product).to be_invalid
         expect(@product.errors[:name]).to be_present
       end
-    end
 
-    context 'Tries to save with a nil-value price' do
-      it 'validates price' do
+      it 'validates rejects an empty "price" field' do
         Category.new(name: "test").save
         @category = Category.first.id
         @product = Product.new(name: "test", category_id: @category, quantity: 1, price: nil)
         expect(@product).to be_invalid
         expect(@product.errors[:price]).to be_present
       end
-    end
 
-    context 'Tries to save with a nil-value quantity' do
-      it 'validates quantity' do
+      it 'validates rejects an empty "quantity" field' do
         Category.new(name: "test").save
         @category = Category.first.id
         @product = Product.new(name: "name", category_id: @category, quantity: nil, price: 1)
         expect(@product).to be_invalid
         expect(@product.errors[:quantity]).to be_present
       end
-    end
 
-    context 'Tries to save with a nil-value category' do
-      it 'validates category' do
+      it 'validates rejects an empty "category" field' do
         @product = Product.new(name: "name", category_id: nil, quantity: 1, price: 1)
         expect(@product).to be_invalid
         expect(@product.errors[:category]).to be_present
