8cf271454c562559530384569ef5cea83ea18fa2
Matthew <m.bednarski@gmail.com>
2022-03-16 02:43:35 +0000

    Add feature test to confirm that clicking on a product shows details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product detail page", type: :feature, js: true do
+  
+   # SETUP
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They click a product to view details" do
+    # ACT
+    visit root_path
+    first('.product').click_link('Details')
+    # DEBUG
+    # save_screenshot
+
+    # VERIFY
+    expect(page).to have_css '.product-detail', count: 1
+  end
+end
