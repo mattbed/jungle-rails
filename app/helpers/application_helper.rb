bbca099784c975b130c96ce7e36a9b430ffd887a
Matthew <m.bednarski@gmail.com>
2022-03-17 03:19:54 +0000

    Reformat 'sold out' functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  def sold_out(product)
+    if product.quantity == 0
+      content_tag(:aside, content_tag(:h5, content_tag(:strong, "SOLD OUT!")), class: "soldout")
+    end
+  end
 end
